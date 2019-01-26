def941b6d74d9745b83c99b020b98e2da1bb56c1
myusername <rodriguez.jamie@live.com>
2019-01-25 21:24:03 -0500

    Updates

---
+++ precache-manifest.7b09e62252e25eccbc05749a8ae1e7aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11525fd074178a5559c0",
+    "url": "/Clicky/static/js/runtime~main.11525fd0.js"
+  },
+  {
+    "revision": "af2434ee867f79117a4b",
+    "url": "/Clicky/static/js/main.af2434ee.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Clicky/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "af2434ee867f79117a4b",
+    "url": "/Clicky/static/css/main.7e854b6c.chunk.css"
+  },
+  {
+    "revision": "f8981cae7c3ed1568d1074f46282de1e",
+    "url": "/Clicky/index.html"
+  }
+];
