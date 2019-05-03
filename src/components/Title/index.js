fc09e826bb9c5032da4ff43ce4803ce8db708d78
Jamie Rodriguez <rodriguez@tellermate-us.com>
2019-05-03 07:28:08 -0400

    corrected typos

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Title(props) {
   return <div className="title">
-          <h1>To Start, click on a beer when ready! Win the game by drinking all the beers, but only drink them once.
+          <h1>To start, click on any beer when ready! Win the game by drinking all the beers, but only drink them once.
              You loose if you drink the same beer twice. Have fun.</h1>
             <p id="winner">{
               props.children
